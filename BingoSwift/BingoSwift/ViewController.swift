36f457bcd9e26a61c731519a817ad4111aeb7327
iyxin <iyxin@iyxinmatomac-mini.local>
2016-09-09 17:05:16 +0800

    1.第一次提交代码

---
+++ BingoSwift/BingoSwift/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  BingoSwift
+//
+//  Created by iexin on 16/9/9.
+//  Copyright © 2016年 刘世兵. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        print("测试")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
